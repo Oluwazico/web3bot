4df0c3006eda96f5b74bb88fbd380977560437df
Oluwazico <105048220+Oluwazico@users.noreply.github.com>
2026-05-14 02:01:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import os
 # Make sure all imports resolve
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from bot.main_bot import main
+from main_bot import main
 
 if __name__ == "__main__":
     main()
